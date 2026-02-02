b20213af0aeb864cdb7c6d16f14a260cf90c353d
Nidvorai4 <basay-ne@bk.ru>
2026-02-02 15:28:09 +0300

    initial

---
+++ test_debug/main.cpp
@@ -0,0 +1,27 @@
+#include "Basay_WiFi_conn.h"
+
+void setup() {
+    Serial.begin(115200);
+
+    // Запуск менеджера. Он сам решит: выйти в онлайн или поднять точку доступа.
+    bool isOnline = BasayWiFi.begin("My_Cool_Project", "Basay_ESP", "иди в хуй ");
+
+    if (isOnline) {
+        // РЕЖИМ С ВАЙФАЕМ: Добавляем ваши датчики из первого сообщения
+        server.on("/get_sensors", HTTP_GET, [](AsyncWebServerRequest *r){
+             r->send(200, "application/json", "{\"temp\": 25.4}"); 
+        });
+    }
+
+    server.begin();
+}
+
+void loop() {
+    BasayWiFi.handle(); // Важно для Captive Portal
+
+    // Сброс настроек при зажатии кнопки (напр. GPIO 9)
+    /* if (digitalRead(9) == LOW) { 
+        delay(3000); 
+        if (digitalRead(9) == LOW) BasayWiFi.resetSettings(); 
+    } */
+}
